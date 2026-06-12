1695868cd8adcdcb867662eb540cdf41f2a27ed5
raichur-arun <raichur.arun23@gmail.com>
2026-06-12 14:43:53 +0530

    fix cors issue

--- src/main/java/com/student/app/controller/StudentController.java
+++ src/main/java/com/student/app/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import com.student.app.entity.StudentEntity;
 import com.student.app.service.StudentService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "*")
 public class StudentController {
 
 	@Autowired
